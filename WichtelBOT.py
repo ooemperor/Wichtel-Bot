ea12fe9b125b1fd4985a625b9bdd139305cd4f16
ooemperor <mi.kaiser@bluewin.ch>
2021-10-31 20:41:12 +0100

    Update WichtelBOT.py

--- WichtelBOT.py
+++ WichtelBOT.py
@@ -7,7 +7,6 @@ Created on Sat Oct 30 16:10:11 2021
 
 import random
 import smtplib
-import ssl
 import copy
 
 """
@@ -26,23 +25,25 @@ SMTP_SERVER = config[2]
 
 def sendMail(receiver, subject, msg):
     #Send a Mail to the receiver with message msg and subject
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(SMTP_SERVER, 465, context = context) as server: # only works with ssl encryption
+    #context = ssl.create_default_context()
+    with smtplib.SMTP(SMTP_SERVER, 587) as server:
         server.login(MAIL_ADRESS, PASSWORD)
-        message_text = ("Subject: " + subject + "\n\n" + msg)
+        message_text = ("From: " + MAIL_ADRESS + "\n" + "To: " + receiver + "\n" + "Subject: " + subject + "\n\n" + msg)
         server.sendmail(MAIL_ADRESS, receiver, message_text)
 
-def wichteln():
+def wichteln_list_generator():
     #members is a list of all participants
     #creates a second list with who is wichtel for who
-    members = ["1 2", "3 4"]
+    members = [] #You need to put Members in here
     members2 = copy.deepcopy(members)
     list_wichtel = []
+    
     for i in range(len(members)):
         rand = random.randint(0, len(members2)-1)
         list_wichtel.append(members2[rand])
         members2.pop(rand)
     return (members, list_wichtel)
+
 
 def check_lists_wichtel(list1, list2):
     #Compares two lists if two elements at the same position are identical
@@ -57,16 +58,20 @@ def check_lists_wichtel(list1, list2):
     else:
         return(404)
 
-def loop():
+
+def wichteln():
     #Function to check if no one will have himself for the Wichteln
     while True:
-        (list1, list2) = wichteln()
+        (list1, list2) = wichteln_list_generator()
         if check_lists_wichtel(list1, list2) == 0:
             print(list1)
             print(list2)
+            for i in range(0, len(list1)):
+                sendMail(get_mail_from_name(list1[i]), "betreff", "message)
             break
         else:
             continue
+        
         
 def get_mail_from_name(name):
     #Takes name in form of Surname Name as input
@@ -78,6 +83,6 @@ def get_mail_from_name(name):
 #Starts all the needed functions
 
 #wichteln()
-sendMail("your mail adress", "testing", "Hello, this is a test for the sending!")
-loop()
-get_mail_from_name("5 6")
+#sendMail("mail", "theme", "message")
+wichteln()
+print(get_mail_from_name("5 6"))
